d636a60abadb6404502079d2f9d1abb327790735
josecruzado21 <josecruzado2103@gmail.com>
2025-08-03 15:06:26 -0700

    feat: change scoreing

--- egs2/asr_dro/asr1/exp_002_auto_baseline.mk
+++ egs2/asr_dro/asr1/exp_002_auto_baseline.mk
@@ -32,7 +32,7 @@ COMMON_EVAL_ARGS=\
 	--exp_dir $(EXP_DIR)/asr_train_$(subst eval_,,$@)/decode_asr_asr_model_valid.loss.best/org/dev_1h_lid/score_cer/
 
 EVAL_CMD=\
-	./local/score_macro.sh $(COMMON_EVAL_ARGS) > results/$(EXPERIMENT_ID)/$@.txt
+	./local/score.sh $(COMMON_EVAL_ARGS) > results/$(EXPERIMENT_ID)/$@.txt
 
 SCEB_PARAMS=\
 	--batch_type language 
