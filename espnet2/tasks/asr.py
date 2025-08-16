a54ab56a2dfe44f8f437a395eb80004f0b841849
josecruzado21 <josecruzado2103@gmail.com>
2025-08-15 18:14:08 -0700

    feat: prints

--- espnet2/tasks/asr.py
+++ espnet2/tasks/asr.py
@@ -610,6 +610,7 @@ class ASRTask(AbsTask):
 
         # 7. Build model
         try:
+            print("argumentos todos": args)
             print("encontro modelo",args.model)
             model_class = model_choices.get_class(args.model)
         except AttributeError:
