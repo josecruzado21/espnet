c14d11e749acc32bc14b20a975914249b53ca54a
josecruzado21 <josecruzado2103@gmail.com>
2025-08-16 17:09:18 -0700

    feat: adding prints for reference

--- espnet2/asr/espnet_model.py
+++ espnet2/asr/espnet_model.py
@@ -591,10 +591,10 @@ class ESPnetASRModel(AbsESPnetModel):
         
         # Calc CER using CTC
         cer_ctc = None
-        print("self.training", self.training)   
-        print("self.error_calculator", self.error_calculator)
-        if not self.training and self.error_calculator is not None:
-            print("Inside the CER calculation")
+        if self.error_calculator is not None:
+            print("Inside CER calculation")
+            print("ys_hat", ys_hat[0:10])
+            print("ys_pad", ys_hat[0:10])
             ys_hat = self.ctc.argmax(encoder_out).data
             cer_ctc = self.error_calculator(ys_hat.cpu(), ys_pad.cpu(), is_ctc=True)
         print("Calculated CER_CTC", cer_ctc)
