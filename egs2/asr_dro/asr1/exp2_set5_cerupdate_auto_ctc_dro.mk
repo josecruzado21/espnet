2b3ea8955848e55175253b9a9047b59575ea2f94
Jose Cruzado <josecruzado2103@gmail.com>
2025-10-22 18:18:59 -0500

    feat: ctc dro expeirments

--- egs2/asr_dro/asr1/exp2_set5_cerupdate_auto_ctc_dro.mk
+++ egs2/asr_dro/asr1/exp2_set5_cerupdate_auto_ctc_dro.mk
@@ -80,6 +80,7 @@ activate-venv:
 	source ../../../tools/activate_python.sh 
 
 MMS_LOSS_CTC_0.001_LA_1.0_ALEB_ARGS= --asr_config conf/$(EXPERIMENT_ID)/mms_example_ctc_dro_0.001_la_1.0.yaml
+MMS_LOSS_CTC_0.01_LA_1.0_ALEB_ARGS= --asr_config conf/$(EXPERIMENT_ID)/mms_example_ctc_dro_0.01_la_1.0.yaml
 
 train_asr_mms_aleb_dro_0.001_la_1.0:
 	./run_multi.sh $(COMMON_TRAIN_ARGS) $(MMS_LOSS_CTC_0.001_LA_1.0_ALEB_ARGS) $(ALEB_PARAMS)
@@ -88,4 +89,13 @@ eval_dev_asr_mms_aleb_dro_0.001_la_1.0: results/$(EXPERIMENT_ID)/
 	$(EVAL_CMD_DEV)
 
 eval_test_asr_mms_aleb_dro_0.001_la_1.0: results/$(EXPERIMENT_ID)/
+	$(EVAL_CMD_TEST)
+
+train_asr_mms_aleb_dro_0.01_la_1.0:
+	./run_multi.sh $(COMMON_TRAIN_ARGS) $(MMS_LOSS_CTC_0.01_LA_1.0_ALEB_ARGS) $(ALEB_PARAMS)
+
+eval_dev_asr_mms_aleb_dro_0.01_la_1.0: results/$(EXPERIMENT_ID)/
+	$(EVAL_CMD_DEV)
+
+eval_test_asr_mms_aleb_dro_0.01_la_1.0: results/$(EXPERIMENT_ID)/
 	$(EVAL_CMD_TEST)
