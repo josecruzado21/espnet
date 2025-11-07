dae9981378df6accb8244cee462713ece137e39c
Jose Cruzado <josecruzado2103@gmail.com>
2025-11-06 23:40:38 -0600

    feat: 0.001 in experiments

--- egs2/asr_dro/asr1/exprep2_cer_set5_auto_group_dro.mk
+++ egs2/asr_dro/asr1/exprep2_cer_set5_auto_group_dro.mk
@@ -79,13 +79,13 @@ results/$(EXPERIMENT_ID)/:
 activate-venv:
 	source ../../../tools/activate_python.sh 
 
-MMS_LOSS_CTC_0.0001_BASE_ARGS= --asr_config conf/$(EXPERIMENT_ID)/mms_example_group_dro_0.0001_base.yaml
+MMS_LOSS_CTC_0.001_BASE_ARGS= --asr_config conf/$(EXPERIMENT_ID)/mms_example_group_dro_0.001_base.yaml
 
-train_asr_mms_aleb_dro_0.0001_base:
-	./run_multi.sh $(COMMON_TRAIN_ARGS) $(MMS_LOSS_CTC_0.0001_BASE_ARGS) $(BASE_PARAMS)
+train_asr_mms_aleb_dro_0.001_base:
+	./run_multi.sh $(COMMON_TRAIN_ARGS) $(MMS_LOSS_CTC_0.001_BASE_ARGS) $(BASE_PARAMS)
 
-eval_dev_asr_mms_aleb_dro_0.0001_base: results/$(EXPERIMENT_ID)/
+eval_dev_asr_mms_aleb_dro_0.001_base: results/$(EXPERIMENT_ID)/
 	$(EVAL_CMD_DEV)
 
-eval_test_asr_mms_aleb_dro_0.0001_base: results/$(EXPERIMENT_ID)/
+eval_test_asr_mms_aleb_dro_0.001_base: results/$(EXPERIMENT_ID)/
 	$(EVAL_CMD_TEST)
